43832f70df077598576514305ef04ec911df5645
kwmaeng <kwmaeng91@gmail.com>
2018-08-16 17:40:56 -0400

    cleaning

--- src/divmodhi3.c
+++ src/divmodhi3.c
@@ -25,8 +25,9 @@
 	 
 
 /* Emulate the division and modulus operation.  */
-/*
-unsigned short udivmodhi4 (unsigned short num, unsigned short den, short modwanted)
+
+unsigned short
+udivmodhi4 (unsigned short num, unsigned short den, short modwanted)
 {
   unsigned short bit = 1;
   unsigned short res = 0;
@@ -50,40 +51,6 @@ unsigned short udivmodhi4 (unsigned short num, unsigned short den, short modwant
   if (modwanted)
     return num;
   return res;
-}
-*/
-unsigned int divhi4 (unsigned int num, unsigned int den)
-{
-  unsigned int bit = 1;
-  unsigned int res = 0;
-
-  while (den < num && bit && !(den & (1 << 31)))
-    {
-      den <<= 1;
-      bit <<= 1;
-    }
-  while (bit)
-    {
-      if (num >= den)
-	{
-	  num -= den;
-	  res |= bit;
-	}
-      bit >>= 1;
-      den >>= 1;
-    }
-
-  return res;
-}
-
-// Simulates multiplication. Note, NOT type safe... this will happily overflow
-unsigned int mulhi4 (unsigned int a, unsigned int b)
-{
-	unsigned int i, accum;
-	for (i = 0, accum = 0; i < b; i++){
-		accum += a;
-	}
-	return accum;
 }
 
 short
@@ -145,48 +112,4 @@ short
 __umodhi3 (short a, short b)
 {
   return udivmodhi4 (a, b, 1);
-}
-
-int __mulsi3 (int a, int b)
-{
-	int neg = 0;
-	int res = 0;
-  if (a < 0)
-    {
-      a = -a;
-      neg = !neg;
-    }
-
-  if (b < 0)
-    {
-      b = -b;
-      neg = !neg;
-    }
-	res = mulhi4(a,b);
-	if (neg)
-		res = -res;
-
-	return res;
-}
-
-int __divsi3 (int a, int b)
-{
-	int neg = 0;
-	int res = 0;
-  if (a < 0)
-    {
-      a = -a;
-      neg = !neg;
-    }
-
-  if (b < 0)
-    {
-      b = -b;
-      neg = !neg;
-    }
-	res = divhi4(a,b);
-	if (neg)
-		res = -res;
-
-	return res;
 }
